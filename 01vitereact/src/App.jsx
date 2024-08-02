6882b7f9c609d39f4e025b377e9c13aa6c6b3758
SowmithSripadi <sowmithsripadi1@gmail.com>
2024-08-02 07:18:27 +0530

    basic test code

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,5 +1,12 @@
+import InnerText from "./inner";
+
 function App() {
-  return <h1>Test</h1>;
+  return (
+    <>
+      <h1>Test</h1>
+      <InnerText />
+    </>
+  );
 }
 
 export default App;
---
+++ 01vitereact/src/inner.jsx
@@ -0,0 +1,5 @@
+function InnerText() {
+  return <h3>This is inner text element</h3>;
+}
+
+export default InnerText;
