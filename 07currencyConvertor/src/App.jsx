211717c809df0c4f496881700af081bb221f9cfc
SowmithSripadi <sowmithsripadi1@gmail.com>
2024-09-12 21:47:54 -0400

    ui changes

--- 07currencyConvertor/src/App.jsx
+++ 07currencyConvertor/src/App.jsx
@@ -4,17 +4,42 @@ import InputBox from "./components";
 function App() {
   return (
     <div
-      className="w-full h-screen flex justify-center items-center "
+      className="w-full h-screen flex justify-center items-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url('https://images.pexels.com/photos/106152/euro-coins-currency-money-106152.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
       }}
     >
-      <form>
-        <InputBox />
-        <button> swap</button>
-        <InputBox />
-        <button> Convert</button>
-      </form>
+      <div className="bg-white w-full max-w-md mx-auto border-2 border-gray-60 rounded-lg p-5 backdrop-blur-3xl bg-white/30">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div className="w-full mb-1">
+            <InputBox />
+          </div>
+          <div className="relative w-full h-0.5">
+            <button
+              className="px-2 py-0.5 rounded-lg absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-white border-2"
+              style={{ backgroundColor: "#f1e9d2" }}
+              type="button"
+            >
+              swap
+            </button>
+          </div>
+          <div className="w-full mt-1 mb-3">
+            <InputBox />
+          </div>
+          <button
+            className="px-8 py-4 rounded-lg w-full border-2 border-white"
+            style={{ backgroundColor: "#f1e9d2" }}
+            type="submit"
+          >
+            {" "}
+            Convert
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- 07currencyConvertor/src/components/InputBox.jsx
+++ 07currencyConvertor/src/components/InputBox.jsx
@@ -4,12 +4,18 @@ function InputBox({ className = "" }) {
   return (
     <div className={`bg-white flex p-3 rounded-lg text-sm ${className}`}>
       <div className="w-1/2">
-        <label className="">From</label>
-        <input type="number" className="w-full text-black/40 mb-2" value="0" />
+        <label className="outline-none w-full bg-transparent py-1.5">
+          From
+        </label>
+        <input
+          type="number"
+          className="w-full text-black/40 mb-2 mt-4 py-1.5 max-w-40 bg-gray-100 rounded-lg pl-2"
+          value="0"
+        />
       </div>
-      <div className="w-1/2">
-        <p>Select Currency</p>
-        <select>
+      <div className="w-1/2 flex justify-end flex-wrap text-right">
+        <p className="w-full">Select Currency</p>
+        <select className="px-1.5 bg-gray-200 py-0.5 rounded-lg outline-none cursor-pointer mr-3">
           <option>usd</option>
           <option>inr</option>
         </select>
